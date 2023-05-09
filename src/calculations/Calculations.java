fb6f7f1a4836a3326f1bf9740449e27e2ea8c851
Kelan Blash <kjblash13@gmail.com>
2023-05-09 15:41:11 -0400

    calculation assignment commit

--- src/calculations/Calculations.java
+++ src/calculations/Calculations.java
@@ -1,2 +1,20 @@
-package com.scanner.calculations;public class Calculations {
+package calculations;
+import java.util.Scanner;
+public class Calculations {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number:");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter another number:");
+        int num2 = input.nextInt();
+
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2) );
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2) );
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2) );
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2) );
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2) );
+
+    }
 }
